b1bf94ce40fbf1d2eae9a6a82bc7917022aa4327
Animesh239 <animeshbarik239@gmail.com>
2024-06-23 22:35:55 +0530

    Refactor footer links for better user experience

--- components/footer/footerContent/FooterColumn.jsx
+++ components/footer/footerContent/FooterColumn.jsx
@@ -4,11 +4,11 @@ import { footerContent } from "../../../mock/footer";
 
 const FooterColumns = () => {
   return (
-    <div className="flex justify-between flex-wrap flex-grow min-width-[800px] xl:rtl:pl-60">
+    <div className="flex justify-between flex-wrap flex-grow min-width-[800px]">
       {footerContent.map((item) => {
         return (
           <div className="mt-6 md:mt-0" key={item.title}>
-            <h2 className="text-md rtl:border-r-4 ltr:border-l-4 border-palette-primary px-2">
+            <h2 className="text-md border-l-4 border-palette-primary px-2">
                 {item.title}
             </h2>
             <div className="flex flex-col mt-2">
