e9a68f542763add12874b054ea4e76cfb05741b3
Animesh239 <animeshbarik239@gmail.com>
2024-06-25 14:20:07 +0530

    added benefits

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Carousel from "@/components/carousel";
+import Benefits from "@/components/Benefits";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Home() {
   return (
     <div>
       <Carousel />
+      <Benefits />
     </div>
   );
 }
